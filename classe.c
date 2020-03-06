645fd2e7420b66e4608937e1c5b156dbfe9bef11
vinirangel <56176228+vinirangel@users.noreply.github.com>
2020-03-06 01:01:19 -0300

    Add files via upload
    .c Class v3

--- classe.c
+++ classe.c
@@ -10,31 +10,84 @@
 #define TAMLINHA   1000
 
 struct personagem{
-
-char* file;
-char* nome;
-int altura;
-double peso;
-char* corDoCabelo;
-char* corDaPele;
-char* corDosOlhos;
-char* anoNascimento;
-char* genero;
-char* homeworld;   
+	char* file;
+	char* nome;
+	int altura;
+	double peso;
+	char* corDoCabelo;
+	char* corDaPele;
+	char* corDosOlhos;
+	char* anoNascimento;
+	char* genero;
+	char* homeworld;   
 }personagem;
+
+/*char* split(char* begin, char* end, char* texto)
+{
+	char* tmp = strdup(texto);
+	while( tmp[strchr(tmp, 
+}*/
 
 void lerArquivo(char* filename, struct personagem *personagem)
 {
-    FILE* arquivo = fopen(filename, "rt");
-    int buffer_size = 3000;
-    //char* buffer[500] = (char *)malloc(buffer_size*sizeof(char*));
+	filename[strlen(filename) - 1] = '\0';
+	FILE* arquivo = fopen(filename, "rt");
+	//int buffer_size = 5000;
+	char* buffer = (char*) malloc(sizeof(char*)*1000);
+	char* tmp = (char*) malloc(sizeof(char*)*1000);
 
-    do{
-    getline(&personagem->file,&buffer_size,arquivo);
-    } while(!feof(arquivo));
+	do{
+		fscanf(arquivo,"%s", buffer);
+		strcat(tmp, buffer);
+	} while(!feof(arquivo) && strstr(buffer, "'films'") == NULL);
+	
+	personagem->file = strdup(tmp);
+	//printf("\n%s", personagem->file);
+	fclose(arquivo);
+	free (buffer);
+	free (tmp);
+}
 
-    fclose(arquivo);
-    printf("%s\n",personagem->file);
+void setNome(struct personagem *personagem)
+{
+	char* cpy = strdup(personagem->file); //(char*) malloc(sizeof(char*)*1000);
+	char* tmp = strtok(cpy,":");
+	
+	/*while(tmp != NULL && strstr(tmp,"',") != NULL )
+	{
+		tmp = strtok(NULL,"'");
+	}*/
+	for(int x = 0; x < 1; x++)
+	{
+		tmp = strtok(NULL,"'");
+	}
+	//printf("\n%s",tmp);
+		//tmp = strtok(NULL,"'");
+	/*int x = 0;
+	while(tmp != NULL && strstr(tmp,"'height'") == NULL )
+	{
+		printf("\nENTREI NO LOOP");
+		printf("\n%d",x);
+		tmp = strtok(NULL,"'");
+		x++;
+	}
+	tmp = strtok(NULL,"'");*/
+	personagem->nome = tmp;
+	printf("\n%s",personagem->nome);
+	
+	/*do{
+	strcat(tmp, personagem->file[IndexOf(		//strstr(personagem->file, ":"))
+	}while(strstr(personagem->file,",") == NULL);*/
+}
+
+void setHeight(struct personagem *personagem)
+{
+	char* cpy = strdup(personagem->file); //(char*) malloc(sizeof(char*)*1000);
+	char* tmp = strtok(cpy,":");
+	for(int x = 0; x < 3; x++)
+	{
+		tmp = strtok(NULL,"'");
+	}
 }
 
 bool isFim(char* s){
@@ -45,11 +98,13 @@ int main(int argc, char** argv)
 {
 	char entrada[NUMENTRADA][TAMLINHA];
 	int numEntrada = 0;
-    
-    do {
+
+	do {
 		fgets(entrada[numEntrada], TAMLINHA, stdin);
 	} while (isFim(entrada[numEntrada++]) == false);
 	numEntrada--;
-    
-    lerArquivo(entrada[0], &personagem);
+	//printf("\n%s",entrada[0]);
+	lerArquivo(entrada[0], &personagem);
+	printf("\n%s", personagem.file);
+	setNome(&personagem);
 }
